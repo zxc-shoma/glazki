c0ee055a107ce61346afd526d38d368a2ddcea24
shoma <zlonamerenniy@yandex.ru>
2023-11-01 14:59:14 +0530

    12 лаба

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -18,8 +18,26 @@ namespace garipov_glazki
     /// <summary>
     /// Логика взаимодействия для ServicePage.xaml
     /// </summary>
+    /// 
+
+
+    
+
     public partial class ServicePage : Page
     {
+
+
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        public List<Agent> CurrentPageList = new List<Agent>();
+        public List<Agent> TableList;
+        
+
+
+
+
         public ServicePage()
         {
             InitializeComponent();
@@ -27,6 +45,7 @@ namespace garipov_glazki
             var currentServices = Garipov_glazkiEntities.GetContext().Agent.ToList();
 
             ServiceListView.ItemsSource = currentServices;
+            UpdateAgents();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -49,6 +68,104 @@ namespace garipov_glazki
         private void FilterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateAgents();
+
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountRecords %10 >0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage>=0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage *10; i<min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage >0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage -1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+
+
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    
+                }
+                
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+
+                for ( int i =1; i<= CountPage;i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                ServiceListView.ItemsSource = CurrentPageList;
+                ServiceListView.Items.Refresh();
+
+
+            }
+
+
+
+
 
         }
 
@@ -99,6 +216,9 @@ namespace garipov_glazki
 
             ServiceListView.ItemsSource = currentAgents;
 
+            TableList = currentAgents;
+            ChangePage(0, 0);
+
 
 
 
@@ -119,6 +239,23 @@ namespace garipov_glazki
         {
             UpdateAgents();
 
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
